c49b75665a293646fb31b4c323c86261fd329235
BoTLeFt <erik_shahov@mail.ru>
2019-05-31 04:22:54 +0300

    Code style added

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <string>
-using namespace std;
+<<<<<<< HEAD
+<<<<<<< HEAD
 int main(){
     string name;
-    sin >> name;
-    cout << "Hello world from " << name;
+    std::cin >> name; //ВВОД
+    std::cout << "Hello world from " << name; //ВЫВОД
     return 0;
+=======
+=======
+>>>>>>> 7d6b7955992a0e9a51239fdf461d51ce302adf79
+using namespace std;
+int
+main()
+{
+  string name;
+<<<<<<< HEAD
+  cin >> name;
+  cout << "Hello world from " << name;
+  return 0;
+>>>>>>> 7th
+=======
+  cin >> name;// ввод
+  cout << "Hello world from " << name; // вывод
+  return 0;
+>>>>>>> 7d6b7955992a0e9a51239fdf461d51ce302adf79
 }
